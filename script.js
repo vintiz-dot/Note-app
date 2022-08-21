1318dfae56351a74f4f98cbd4e1792392a259c73
vintiz-dot <vintizfjes@gmail.com>
2022-08-21 07:05:59 -0400

    implemented delete

--- script.js
+++ script.js
@@ -80,57 +80,52 @@ class App {
     this.#btnSubmit.addEventListener("click", this._createPost_.bind(this));
   }
 
-  #createNewPost(title, post) {
-    if (post === "" || title === "") return;
+  /***************************BEGINNING OF MAIN FUNCTIONS*************************** */
 
-    const newPost = {
-      author: this.#activeUser.username,
-      title: title,
-      msg: post,
-      time: this.#setPostTime(),
-      postId: this.#activeUser.postId,
-    };
-    this.#activeUser.postId += 1;
-    this.#submitPost(newPost);
+  _addNewUser(x) {
+    x.preventDefault();
 
-    return newPost;
-  }
-  #findPost(postId) {
-    return this.#activeUserPost.find((post) => post.postId === postId);
-  }
-  #setPostTime() {
-    return `${new Date().toDateString()} at ${new Date().toLocaleTimeString()}`;
-  }
-  #submitPost(post) {
-    this.#activeUserPost.push(post);
-    this.#activeUser.notesContainer.push(post);
-    localStorage.setItem(
-      this.#activeUser.username,
-      JSON.stringify(this.#activeUserPost)
-    );
+    // collect entires
+    const username = document.getElementById("Signup-username");
+    const password = document.getElementById("SignupPassword");
+
+    // gaurd for valid inputs
+    if (
+      username.value === NaN ||
+      username.value === undefined ||
+      username.value === null ||
+      password.value.length < 7
+    ) {
+      this.#invalidMsg("invaid entries");
+      return;
+    }
+
+    // reserved username
+    if (username.value.toLowerCase() === "towel") {
+      this.#invalidMsg(`Boss that username is reserved 😂😂😂😂`);
+      username.value = password.value = "";
+      return;
+    }
+
+    // check for existing username
+    if (this._findUser(username.value)) {
+      this.#invalidMsg(`Username already exists`);
+      return;
+    }
+
+    // initialize user and setup display
+    const user = new User(username.value, password.value);
+    this.#users.push(user);
+    this.#activeUser = user;
+    this.#setupDisplay();
+    username.value = password.value = this.#invalid.innerHTML = "";
+    this.#displayPosts();
+
+    // update local storage
+    localStorage.setItem("users", [JSON.stringify(this.#users)]);
   }
 
-  #updateUsers() {
-    const users = JSON.parse(localStorage.getItem("users"));
-    if (!users) return;
-    users.forEach((user) => this.#users.push(user));
-  }
-  _findUser(username) {
-    return this.#users.filter((user) => user.username === username).length === 0
-      ? false
-      : this.#users.filter((user) => user.username === username);
-  }
-
-  #rebuildOldPosts() {
-    const data = JSON.parse(localStorage.getItem(this.#activeUser.username));
-    if (!data) return;
-    data.forEach((postObj) => this.#activeUserPost.push(postObj));
-  }
-  #setupDisplay() {
-    this.#landingPage.classList.add("hidden");
-    this.#userPage.classList.remove("hidden");
-    this.#navBar.classList.remove("hidden");
-  }
+  /************************END OF THE CREATE NEW USER************************* */
 
   _initialize(c) {
     c.preventDefault();
@@ -143,7 +138,7 @@ class App {
     //check if the username exists
     if (!this._findUser(username.value)) {
       this.#invalid_login.classList.remove("hidden");
-      username = password = "";
+      username.value = password.value = "";
       return;
     }
 
@@ -165,11 +160,14 @@ class App {
     this.#displayPosts();
   }
 
+  // *********************END OF INITIALIZE METHOD****************************//
+
   #logOut() {
     // deactivate the active user
     this.#activeUser = null;
     this.#activeUserPost = [];
     this._clearnotes();
+
     // turn off the display
     this.#navBar.classList.add("hidden");
     this.#landingPage.classList.remove("hidden");
@@ -178,81 +176,131 @@ class App {
     this.#invalid.classList.add("hidden");
   }
 
+  // **************************END OF LOGOUT METHOD*************************** //
+
+  #updateUsers() {
+    const users = JSON.parse(localStorage.getItem("users"));
+    if (!users) return;
+    users.forEach((user) => this.#users.push(user));
+  }
+  //****************END OF UPDATE USERS******************************** */
+
+  #createNewPost(title, post) {
+    if (post === "" || title === "") return;
+
+    const newPost = {
+      author: this.#activeUser.username,
+      title: title,
+      msg: post,
+      time: this.#setPostTime(),
+      postId: this.#activeUser.postId,
+    };
+    this.#activeUser.postId += 1;
+    this.#submitPost(newPost);
+
+    return newPost;
+  }
+
+  /* *************************END OF CREATE NEW POST************************** */
+
+  #submitPost(post) {
+    this.#activeUserPost.push(post);
+    this.#activeUser.notesContainer.push(post);
+    localStorage.setItem(
+      this.#activeUser.username,
+      JSON.stringify(this.#activeUserPost)
+    );
+  }
+  /* ***************END OF SUBMIT POST****************************** */
+
+  #rebuildOldPosts() {
+    const data = JSON.parse(localStorage.getItem(this.#activeUser.username));
+    if (!data) return;
+    data.forEach((postObj) => this.#activeUserPost.push(postObj));
+  }
+
+  /*******************END OF THE REBUILD OLD POST******************* */
+
+  #editPost(x) {
+    this._closeModal();
+    if (!x.target.classList.contains("edit-btn")) return;
+    target = x.target;
+    const btnSubmit = document.getElementById("edit-post-btn");
+    this._toggleHidden();
+    privateContainer.scrollIntoView({
+      block: "center",
+      behavior: "auto",
+      inline: "start",
+    });
+    this._clearValues();
+    btnSubmit.addEventListener("click", this._editPost_.bind(this));
+  }
+
+  // *************************END OF EDIT POST*************************//
+
+  #deletePost(x) {
+    if (!x.target.classList.contains("delete-btn")) return;
+    const deleteIndex = this.#activeUserPost.findIndex((post) => {
+      return post.postId === +x.target.previousElementSibling.id;
+    });
+    this.#activeUserPost.splice(deleteIndex, 1);
+    localStorage.setItem(
+      this.#activeUser.username,
+      JSON.stringify(this.#activeUserPost)
+    );
+    this.#displayPosts();
+  }
+  // ***************END OF DELETE POST METHOD************************
+  /**
+   *
+   *
+   *
+   *
+   * =================>END OF MAIN FUNCTIONS<========================
+   *
+   *
+   *
+   */
+
+  /*********************BEGINNING OF HELPER FUNCTIONS********************* */
+  #setPostTime() {
+    return `${new Date().toDateString()} at ${new Date().toLocaleTimeString()}`;
+  }
+
+  #setupDisplay() {
+    this.#landingPage.classList.add("hidden");
+    this.#userPage.classList.remove("hidden");
+    this.#navBar.classList.remove("hidden");
+  }
+
+  #findPost(postId) {
+    return this.#activeUserPost.find((post) => post.postId === postId);
+  }
+  _findUser(username) {
+    return this.#users.filter((user) => user.username === username).length === 0
+      ? false
+      : this.#users.filter((user) => user.username === username);
+  }
+
   #loadSignupPage() {
     const btnSignUp = document.getElementById("offer-sign-up");
     btnSignUp.addEventListener("click", this._loadSignUP.bind(this));
   }
+
   _loadSignUP(x) {
     x.preventDefault();
     this.#siginPage.classList.add("hidden");
     this.#signupPage.classList.remove("hidden");
   }
-  #invalidMsg(msg) {
-    this.#invalid.innerHTML = "";
-    this.#invalid.classList.remove("hidden");
-    this.#invalid.insertAdjacentText("beforeend", msg);
-  }
 
-  _addNewUser(x) {
-    x.preventDefault();
+  // CREATE POST HELPER FUNCTIONS
 
-    // collect entires
-    username = document.getElementById("Signup-username").value;
-    password = document.getElementById("SignupPassword").value;
-
-    // gaurd for valid inputs
-    if (
-      username === NaN ||
-      username === undefined ||
-      username === null ||
-      password.length < 7
-    ) {
-      this.#invalidMsg("invaid entries");
-      return;
-    }
-
-    // reserved username
-    if (username.toLowerCase() === "towel") {
-      this.#invalidMsg(`Boss that username is reserved 😂😂😂😂`);
-      return;
-    }
-
-    // check for existing username
-    if (this._findUser(username)) {
-      this.#invalidMsg(`Username already exists`);
-      return;
-    }
-
-    // initialize user and setup display
-    const user = new User(username, password);
-    this.#users.push(user);
-    this.#activeUser = user;
-    this.#setupDisplay();
-    username = password = this.#invalid.innerHTML = "";
-    this.#displayPosts();
-
-    // update local storage
-    localStorage.setItem("users", [JSON.stringify(this.#users)]);
-  }
-
-  #closeModalCreatePost() {
-    const modal = document.getElementById("close-create-modal");
-    modal.addEventListener("click", function (x) {
-      x.preventDefault();
-      const newPostWindow = document.getElementById("create-post-window");
-      newPostWindow.classList.add("hidden");
-      this.#overlay.classList.add("hidden");
-    });
-  }
   #createPost() {
     this.#closeModalCreatePost();
     const newPostbtn = document.getElementById("create-post!");
     newPostbtn.addEventListener("click", this._createPost.bind(this));
   }
-  _clearValues() {
-    document.getElementById("post-create-msg").value = "";
-    document.getElementById("create-title").value = "";
-  }
+
   _createPost_(x) {
     x.preventDefault();
     let newtext = document.getElementById("post-create-msg").value;
@@ -278,18 +326,27 @@ class App {
     });
   }
 
-  #deletePost(x) {
-    if (!x.target.classList.contains("delete-btn")) return;
-    const deleteIndex = this.#activeUserPost.findIndex((post) => {
-      return post.postId === +x.target.previousElementSibling.id;
+  #closeModalCreatePost() {
+    const modal = document.getElementById("close-create-modal");
+    modal.addEventListener("click", function (x) {
+      x.preventDefault();
+      const newPostWindow = document.getElementById("create-post-window");
+      newPostWindow.classList.add("hidden");
+      this.#overlay.classList.add("hidden");
     });
-    this.#activeUser.notes.splice(deleteIndex, 1);
-    this.#displayPosts();
   }
 
-  _toggleHidden() {
-    this.#overlay.classList.toggle("hidden");
-    privateContainer.classList.toggle("hidden");
+  //END OF CREATE POST HELPER FUNCTIONS
+
+  _clearValues() {
+    document.getElementById("post-create-msg").value = "";
+    document.getElementById("create-title").value = "";
+  }
+
+  #invalidMsg(msg) {
+    this.#invalid.innerHTML = "";
+    this.#invalid.classList.remove("hidden");
+    this.#invalid.insertAdjacentText("beforeend", msg);
   }
 
   _editPost_() {
@@ -301,23 +358,61 @@ class App {
     privateContainer.classList.add("hidden");
     this.#displayPosts();
   }
+
   _closeModal() {
     const closeModal = document.querySelector(".close-modal");
     closeModal.addEventListener("click", this._toggleHidden.bind(this));
   }
-  #editPost(x) {
-    this._closeModal();
-    if (!x.target.classList.contains("edit-btn")) return;
-    target = x.target;
-    const btnSubmit = document.getElementById("edit-post-btn");
-    this._toggleHidden();
-    privateContainer.scrollIntoView({
-      block: "center",
-      behavior: "auto",
-      inline: "start",
-    });
-    this._clearValues();
-    btnSubmit.addEventListener("click", this._editPost_.bind(this));
+
+  _clearnotes() {
+    noteHead.innerHTML = "";
+    noteBody.innerHTML = "";
+  }
+
+  _toggleHidden() {
+    this.#overlay.classList.toggle("hidden");
+    privateContainer.classList.toggle("hidden");
+  }
+
+  randomNumber(max) {
+    return Math.floor(Math.random() * max);
+  }
+  randomPost(list) {
+    return list[this.randomNumber(list["length"])];
+  }
+
+  getjumbotron() {
+    const post = this.randomPost(this.#activeUserPost);
+    if (post?.title === undefined) return;
+    const html = `<div class="p-5 mb-4 bg-light rounded-3">
+    <div class="container-fluid py-5">
+      <h1 class="display-5 fw-bold">${post?.title}</h1>
+      <p class=" fs-4">${post?.msg}
+      </p>      
+      </div>
+    </div>`;
+    html && noteHead.insertAdjacentHTML("beforeend", html);
+  }
+
+  #displayPosts(where = "beforeend") {
+    this._clearnotes();
+    this.getjumbotron();
+    this._getNotes(where);
+    if (this.#activeUserPost.length === 0) {
+      noteHead.insertAdjacentHTML(
+        where,
+        `<div class="p-5 mb-4 bg-light rounded-3">
+      <div class="container-fluid py-5">
+        <h1 style="
+        font-size: 36px;
+        color: #5fcf80;
+        font-weight: 600;
+        font-family: Poppins, sans-serif;"
+       class="display-5 fw-bold">No Posts <br> Create a New Post Now</h1>             
+        </div>
+      </div>`
+      );
+    }
   }
 
   _getNotes(where) {
@@ -340,49 +435,6 @@ class App {
                 </div>`;
       noteBody.insertAdjacentHTML(where, html);
     });
-  }
-  randomNumber(max) {
-    return Math.floor(Math.random() * max);
-  }
-  randomPost(list) {
-    return list[this.randomNumber(list["length"])];
-  }
-
-  getjumbotron() {
-    const post = this.randomPost(this.#activeUserPost);
-    if (post?.title === undefined) return;
-    const html = `<div class="p-5 mb-4 bg-light rounded-3">
-    <div class="container-fluid py-5">
-      <h1 class="display-5 fw-bold">${post?.title}</h1>
-      <p class=" fs-4">${post?.msg}
-      </p>      
-      </div>
-    </div>`;
-    html && noteHead.insertAdjacentHTML("beforeend", html);
-  }
-  _clearnotes() {
-    noteHead.innerHTML = "";
-    noteBody.innerHTML = "";
-  }
-  #displayPosts(where = "beforeend") {
-    this._clearnotes();
-    this.getjumbotron();
-    this._getNotes(where);
-    if (this.#activeUserPost.length === 0) {
-      noteHead.insertAdjacentHTML(
-        where,
-        `<div class="p-5 mb-4 bg-light rounded-3">
-      <div class="container-fluid py-5">
-        <h1 style="
-        font-size: 36px;
-        color: #5fcf80;
-        font-weight: 600;
-        font-family: Poppins, sans-serif;"
-       class="display-5 fw-bold">No Posts <br> Create a New Post Now</h1>             
-        </div>
-      </div>`
-      );
-    }
   }
 }
 
